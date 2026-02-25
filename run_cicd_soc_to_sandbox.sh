ef05cf2cfd2f114ac441aee3cf28e6656087eeb0
rupertons <rupert.anderson@ext.ons.gov.uk>
2026-02-25 17:53:08 +0000

    fix: no need for cicd project id, use built in cloud build variable instead

--- run_cicd_soc_to_sandbox.sh
+++ run_cicd_soc_to_sandbox.sh
@@ -25,5 +25,5 @@ gcloud beta builds submit . --config=cicd/cloudbuild_dev_and_sandbox.yaml \
 	--project $CICD_PROJECT_ID \
 	--service-account projects/$CICD_PROJECT_ID/serviceAccounts/$CICD_SA \
 	--gcs-source-staging-dir $CB_BUCKET \
-	--substitutions=_GAR_SOC_VECTOR_STORE_IMAGE=$REGION-docker.pkg.dev/$PROJECT_ID/soc-vector-store/soc-vector-store,SHORT_SHA=$GIT_SHA,_TARGET_PROJECT_ID=$PROJECT_ID,_CICD_PROJECT_ID=$CICD_PROJECT_ID,_VECTOR_STORE_DIR=gs://$CR_BUCKET/soc_vector_store_config/data,_ENV_NAME=$ENV_NAME,_API_SA=$API_SA,_SOC_INDEX_FILE=soc2020volume2thecodingindexexcel16042025.xlsx,_SOC_STRUCTURE_FILE=soc2020volume1structureanddescriptionofunitgroupsexcel16042025.xlsx,_API_VERSION=$API_VERSION \
+	--substitutions=_GAR_SOC_VECTOR_STORE_IMAGE=$REGION-docker.pkg.dev/$PROJECT_ID/soc-vector-store/soc-vector-store,SHORT_SHA=$GIT_SHA,_TARGET_PROJECT_ID=$PROJECT_ID,_VECTOR_STORE_DIR=gs://$CR_BUCKET/soc_vector_store_config/data,_ENV_NAME=$ENV_NAME,_API_SA=$API_SA,_SOC_INDEX_FILE=soc2020volume2thecodingindexexcel16042025.xlsx,_SOC_STRUCTURE_FILE=soc2020volume1structureanddescriptionofunitgroupsexcel16042025.xlsx,_API_VERSION=$API_VERSION \
 	--region europe-west2
